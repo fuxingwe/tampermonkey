988b5e081b7f2b1cb3412675aba1e27b6a870b27
fuxing <xing.fu@cytxcn.com>
2024-04-01 21:18:56 +0800

    修改订阅名称

--- dist/hbl_tools.user.sub.js
+++ dist/hbl_tools.user.sub.js
@@ -1,8 +1,8 @@
 // ==UserSubscribe==
-// @name         feng
+// @name         hbl_tools_sub
 // @description  脚本猫订阅hbl_tools系列脚本
 // @version      0.0.1
 // @author       feng
 // @connect      www.baidu.com
-// @scriptUrl    https://update.greasyfork.org/scripts/490843/hbl_tools.user.js
+// @scriptUrl    https://gitee.com/fuxingwe/tampermonkey/raw/main/dist/hbl_tools.user.js
 // ==/UserSubscribe==
