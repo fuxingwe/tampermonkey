a229ea5990d23ff866dafb5ec908776c82e7074d
fuxing <xing.fu@cytxcn.com>
2024-04-01 21:11:38 +0800

    增加脚本猫订阅脚本

---
+++ dist/hbl_tools.user.sub.js
@@ -0,0 +1,8 @@
+// ==UserSubscribe==
+// @name         feng
+// @description  脚本猫订阅hbl_tools系列脚本
+// @version      0.0.1
+// @author       feng
+// @connect      www.baidu.com
+// @scriptUrl    https://update.greasyfork.org/scripts/490843/hbl_tools.user.js
+// ==/UserSubscribe==
